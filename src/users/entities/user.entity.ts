c449f582b00b55a17687d47c98e5b5d5bf2bc05e
davidbarbi3r <davidbarbi3r@gmail.com>
2023-05-01 18:21:50 +0200

    :sparkles: user module maj

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,13 +1,13 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm"
 
 @Entity()
 export class User {
   @PrimaryGeneratedColumn()
-  id: number;
+  id: number
 
   @Column()
-  userName: string;
+  userName: string
 
   @Column({ default: true })
-  isActive: boolean;
+  isActive: boolean
 }
