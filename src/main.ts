b4a3401522816ae6c19f28f6b611b3e6a25410ee
davidbarbi3r <davidbarbi3r@gmail.com>
2023-07-22 15:21:55 +0200

    :sparkles: add swagger to project

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,23 @@ import { NestFactory } from '@nestjs/core';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const port = process.env.PORT || 3000;
 
   const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
+
+  const config = new DocumentBuilder()
+    .setTitle('Old School Oasis API')
+    .setDescription('The Old School Oasis API description')
+    .setVersion('0.1')
+    .addTag('Old School Oasis')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   app.enableCors();
   // global pipes are used to modify the incoming data before it reaches the controller
   // validation pipe is a special one used to validate the incoming data
